b4d41ab0cf149127750319a0afd0b07a82d1007e
Lukas Martincek <lukas.martincek3@gmail.com>
2025-03-07 12:58:49 +0100

    add frontend url to cors

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const app = express();
 // Enable CORS
 app.use(
     cors({
-        origin: 'http://localhost:5173', //TODO Replace with your frontend URL
+        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
         methods: 'GET,POST',
         allowedHeaders: 'Content-Type,Authorization',
     })
